75fabeeb9d166171aa1e7dca4424e9d097e3b349
Ibrahim Duale <ibrahimd@my.yorku.ca>
2022-08-15 16:26:47 -0400

    login page visual and error handling
    implemented the login page, and error handling on invalid input

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { socketContext } from "./SocketContext";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Link } from "react-router-dom";
 const App = () => {
     let { socket } = useContext(socketContext);
-
+    let navigate = useNavigate();
     return (
-        <div>
-            <h1>App Page</h1>
+        <Box sx={{ flexGrow: 1, height: "100vh" }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: "center" }}>
+                        <Button color="inherit" onClick={() => navigate("/")}>Chat Message App</Button>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
             <Outlet />
-        </div>
-    )
+        </Box>
+    );
 }
 
 export default App;
--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,7 +1,49 @@
+import { Box, Typography, TextField, Button } from "@mui/material";
+import { useState } from "react";
+const LoginStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    AlignItems: "center",
+    paddingTop: "15px",
+    maxWidth: "500px",
+    margin: "auto",
+    marginTop: "10%",
+    rowGap: "25px",
+}
 
 const Login = () => {
+    let [username, setUsername] = useState("");
+    let [roomName, setRoomName] = useState("");
+    let [roomNameError, setRoomNameError] = useState({});
+    let [usernameError, setUsernameError] = useState({});
+
+    const joinRoom = (username, roomName) => {
+        console.log(`username : ${username} room name : ${roomName}`)
+        if (!username) {
+            setUsernameError({ error: true, helperText: "Username cannot be blank." })
+        }
+        if (!roomName) {
+            setRoomNameError({ error: true, helperText: "Room Name cannot be blank." });
+        }
+
+        if (roomName && username) {
+            setUsernameError({});
+            setRoomNameError({});
+            //communicate to server and navigate to appropriate room
+            console.log("can communicate to server");
+        }
+    }
+
     return (
-        <div>This is the login Page</div>
+        <Box sx={LoginStyle}>
+            <Typography align="center" variant="h2" component="h1" gutterBottom>
+                Join A Room
+            </Typography>
+            <TextField {...usernameError} onChange={(e) => { setUsername(e.target.value); setUsernameError({}) }} id="username" label="Username" variant="outlined" />
+            <TextField {...roomNameError} id="roomName" onChange={(e) => { setRoomName(e.target.value); setRoomNameError({}); }} label="Room Name" variant="outlined" />
+            <Button onClick={(e) => joinRoom(username, roomName)} variant="contained" size="large">Enter</Button>
+        </Box>
     )
 }
 
